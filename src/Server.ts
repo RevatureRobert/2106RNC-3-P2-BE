7c7ff40af09296759441adbba2c698df4e3e44c4
Kent Smith <kentrsmith1996@gmail.com>
2021-07-19 18:22:56 -0400

    Fixed serverless deployment and several dependency issues, many more dependency issues to fix along with other cleanup.

--- src/Server.ts
+++ src/Server.ts
@@ -1,8 +1,4 @@
-import cookieParser from "cookie-parser";
-import morgan from "morgan";
 import path from "path";
-import helmet from "helmet";
-import serverless from "serverless-http";
 
 import express, {NextFunction, Request, Response} from "express";
 import StatusCodes from "http-status-codes";
@@ -20,17 +16,6 @@ const {BAD_REQUEST} = StatusCodes;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
-
-// Show routes called in console during development
-if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"));
-}
-
-// Security
-if (process.env.NODE_ENV === "production") {
-    app.use(helmet());
-}
 
 // Add APIs
 app.use("/api", BaseRouter);
@@ -58,4 +43,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 
 // Export express instance
 // export default app;
-exports.handler = serverless(app);
+// exports.handler = serverless(app);
--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,14 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-// import "./pre-start"; // Must be the first import
-// import http from "http";
-// import https from "https";
-// import fs from "fs";
-// import app from "@server";
-// import logger from "@shared/Logger";
-
-// Start the server
-// const httpPort = Number(process.env.HTTPPORT || 8080);
-// const httpsPort = Number(process.env.HTTPSPORT || 8081);
-// http.createServer(app).listen(httpPort, () => {
-//     logger.info("Http server started on port: " + httpPort);
-// });
-// https
-//     .createServer(
-//         {
-//             key: fs.readFileSync("server.key"),
-//             cert: fs.readFileSync("server.cert")
-//         },
-//         app
-//     )
-//     .listen(httpsPort, () => {
-//         logger.info("Https server started on port: " + httpsPort);
-//     });
-
-import cookieParser from "cookie-parser";
-import morgan from "morgan";
-import path from "path";
-import helmet from "helmet";
 // @ts-ignore
-import express, {NextFunction, Request, Response} from "@vendia/serverless-express";
+import express, {NextFunction, Request, Response} from "serverless-express/express";
 // @ts-ignore
-import handler from "serverless-express/handler"
-// import actualExpress from "express";
+import serverlessHandler from "serverless-express/handler"
 import StatusCodes from "http-status-codes";
 import "express-async-errors";
-
 import BaseRouter from "./routes";
 import logger from "@shared/Logger";
-// const express = serverlessExpress as actualExpress.Express;
 
-const app = express() //as express1.Application;
+process.env.SERVERLESS_EXPRESS_PLATFORM = "aws";
+const app = express();
 const {BAD_REQUEST} = StatusCodes;
 
 /************************************************************************************
@@ -50,17 +17,6 @@ const {BAD_REQUEST} = StatusCodes;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
-
-// Show routes called in console during development
-if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"));
-}
-
-// Security
-if (process.env.NODE_ENV === "production") {
-    app.use(helmet());
-}
 
 // Add APIs
 app.use("/api", BaseRouter);
@@ -74,18 +30,5 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     });
 });
 
-/************************************************************************************
- *                              Serve front-end content
- ***********************************************************************************/
-
-// const viewsDir = path.join(__dirname, 'views');
-// app.set('views', viewsDir);
-// const staticDir = path.join(__dirname, 'public');
-// app.use(express.static(staticDir));
-// app.get('*', (req: Request, res: Response) => {
-//     res.sendFile('index.html', {root: viewsDir});
-// });
-
 // Export express instance
-export default app;
-// module.exports.handler = serverless(app);
+export const handler = serverlessHandler(app);
