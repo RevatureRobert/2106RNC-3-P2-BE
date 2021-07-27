289c2fd4a3971b001704ca2284799ff7dec89174
mat2718 <matthewterry68.mt@gmail.com>
2021-07-27 16:39:34 -0400

    minor updates

--- src/daos/Messages/MessagesDao.test.ts
+++ src/daos/Messages/MessagesDao.test.ts
@@ -11,7 +11,7 @@ import dotenv from "dotenv";
 (() => {
   // Set the env file
   const result2 = dotenv.config({
-    path: path.join(__dirname, `env/test.env`),
+    path: "../../pre-start/env/test.env"  //path.join(__dirname, `env/test.env`),
   });
   if (result2.error) {
     throw result2.error;
